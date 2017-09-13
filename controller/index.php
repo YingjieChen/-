a850b46a1f6671832799fb1d5b339bb8efe7ab1a
yingjiechen <YingjieChen@live.cn>
2017-09-13 13:55:44 +0800

    js completed

--- controller/index.php
+++ controller/index.php
@@ -3,6 +3,9 @@
 	require_once("./lib/kuaidi.class.php");
 	$type		=	$_GET['type'];
 	$postid		=	$_GET['postid'];
+
+	$postid		=	preg_replace('/[^0-9]*/','',$postid);
+
 	$kuaidi		=	new KuaiDi($type,$postid,$config['api']['kuaidi']);
 	echo json_encode($kuaidi->getdata());
 ?>
